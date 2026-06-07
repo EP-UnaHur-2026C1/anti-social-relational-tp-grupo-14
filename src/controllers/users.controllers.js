402c535e16fb9fc94eb48b13b5f5b6958031a6ae
SNR <santiagonunez1230@gmail.com>
2026-06-07 02:52:22 -0300

    Creacion de controllers y refactorizacion de users.routes

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,59 @@
+const {users} = require("../models")
+
+const getAllUsers = async (req, res) => {
+    const users = await User.findAll()
+    res.json(users)
+}
+
+const getUserById = async (req, res) => {
+    const user = await User.findByPk(req.params.id)
+    if (!user) {
+        return res.status(404).json({
+            error: "Usuario no encontrado"
+        })
+    }
+    res.json(user)
+}
+
+const createUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body)
+        res.status(201).json(user)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            error: error.message,
+            details: error.errors
+        })
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id)
+        if (!user) {
+            return res.status(404).json({
+                error: "Usuario no encontrado"
+            })
+        }
+        await user.update(req.body)
+        res.json(user)
+    } catch (error) {
+        res.status(400).json({
+            error: error.message
+        })
+    }
+}
+
+const deleteUser = async (req, res) => {
+    const user = await User.findByPk(req.params.id)
+    if (!user) {
+        return res.status(404).json({
+            error: "Usuario no encontrado"
+        })
+    }
+    await user.destroy()
+    res.json({
+        message: "Usuario eliminado"
+    })
+}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,75 +2,12 @@ const express = require("express")
 const { User } = require("../models")
 
 const router = express.Router()
+const usersControllers = require("../controllers/users.controllers")
 
-router.get("/", async (req, res) => {
-    const users = await User.findAll()
-    res.json(users)
-})
-
-router.get("/:id", async (req, res) => {
-    const user = await User.findByPk(req.params.id)
-
-    if (!user) {
-        return res.status(404).json({
-            error: "Usuario no encontrado"
-        })
-    }
-
-    res.json(user)
-})
-
-router.post("/", async (req, res) => {
-    try {
-        const user = await User.create(req.body)
-        res.status(201).json(user)
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({
-            error: error.message,
-            details: error.errors
-        })
-    }
-})
-
-router.put("/:id", async (req, res) => {
-    try {
-
-        const user = await User.findByPk(req.params.id)
-
-        if (!user) {
-            return res.status(404).json({
-                error: "Usuario no encontrado"
-            })
-        }
-
-        await user.update(req.body)
-
-        res.json(user)
-
-    } catch (error) {
-
-        res.status(400).json({
-            error: error.message
-        })
-    }
-})
-
-router.delete("/:id", async (req, res) => {
-
-    const user = await User.findByPk(req.params.id)
-
-    if (!user) {
-        return res.status(404).json({
-            error: "Usuario no encontrado"
-        })
-    }
-
-    await user.destroy()
-
-    res.json({
-        message: "Usuario eliminado"
-    })
-})
+router.get("/", usersControllers.getAllUsers)
+router.get("/:id", usersControllers.getUserById)
+router.post("/", usersControllers.createUser)
+router.put("/:id", usersControllers.updateUser)
+router.delete("/:id", usersControllers.deleteUser)
 
 module.exports = router
